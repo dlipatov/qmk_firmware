78248402510382eefbf66f30b63f41ed4b7188f6
Dmitry Lipatov <dmitry.lipatov@gmail.com>
2023-03-30 09:16:27 +0300

    Custom keymap

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dlipatov/config.h
@@ -0,0 +1,58 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+
+#define USE_SERIAL
+
+#define EE_HANDS
+
+// Underglow
+#undef RGBLED_NUM
+#undef RGBLED_SPLIT
+
+#define RGBLIGHT_DISABLE_KEYCODES
+
+#undef RGB_DI_PIN
+#define RGB_DI_PIN D1
+
+#define RGBLED_NUM 24    // Number of LEDs
+#define RGBLED_SPLIT { 12, 12 }
+#define RGBLIGHT_LIMIT_VAL 255
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_DEFAULT_HUE 246
+#define RGBLIGHT_DEFAULT_SAT 255
+#define RGBLIGHT_DEFAULT_VAL RGBLIGHT_LIMIT_VAL
+
+
+// Layers
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define LAYER_STATE_8BIT
+
+//Boogmagic Lite
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+#define BOOTMAGIC_LITE_ROW_RIGHT 6
+#define BOOTMAGIC_LITE_COLUMN_RIGHT 5
+
+//Tap Dance
+#define TAPPING_TERM 200
---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dlipatov/keymap.c
@@ -0,0 +1,120 @@
+/* A standard layout for the Dactyl Manuform 5x6 Keyboard */
+
+#include QMK_KEYBOARD_H
+
+//Tap Dance
+#ifdef TAP_DANCE_ENABLE
+// Tap Dance declarations
+enum {
+    TD_ESC_LGUI,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_LGUI] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_LGUI),
+};
+
+#endif
+
+// Layers
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT_5x6(
+    TD(TD_ESC_LGUI),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                KC_6,   KC_7,   KC_8   ,    KC_9  ,   KC_0,       KC_MINS,
+    KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                        KC_Y,   KC_U,   KC_I   ,    KC_O  ,   KC_P,       KC_BSLS,
+    KC_EQL,     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                        KC_H,   KC_J,   KC_K   ,    KC_L  ,   KC_SCLN,    KC_QUOT,
+    KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                        KC_N,   KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,    KC_RSFT,
+               KC_LEFT , KC_RGHT,                                                KC_LBRC, KC_RBRC,
+                                        KC_BSPC, XXX,                                XXX, KC_SPC,
+                                                    KC_DEL,  KC_LALT,       KC_RALT, KC_ENT,
+                                                    KC_LCTL , LOWER,       RAISE,  KC_RCTL
+  ),
+
+  [_LOWER] = LAYOUT_5x6(
+
+        XXX, XXX, XXX, XXX, XXX, XXX,            KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,   XXX,
+        XXX, XXX, XXX, XXX, XXX, XXX,            XXX, XXX, XXX, XXX, XXX, XXX,
+        XXX, XXX, XXX, XXX, XXX, XXX,            XXX, XXX, XXX, XXX, XXX, XXX,
+        XXX, XXX, XXX, XXX, XXX, XXX,            XXX, XXX, XXX, XXX, XXX , XXX,
+                XXX, XXX,                          KC_F11, KC_F12,
+                            XXX, XXX,                        XXX, XXX,
+                                XXX, XXX,            XXX, XXX,
+                                XXX, XXX,            XXX,  XXX
+
+  ),
+
+  [_RAISE] = LAYOUT_5x6(
+     KC_CAPS, KC_F1,  KC_F2, KC_F3, KC_F4, KC_F5,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,
+     XXX, XXX, XXX, XXX, XXX,  XXX,             XXX, XXX, XXX, XXX, XXX, XXX,
+     KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,               XXX, XXX, XXX, XXX, XXX, XXX,
+     KC_GRV, KC_BSLS, XXX, XXX, XXX, XXX,             XXX,  XXX, XXX, XXX, XXX, XXX,
+
+                        KC_UP, KC_DOWN,                                                  XXX, XXX,
+                                           XXX, XXX,            XXX, XXX,
+                                           XXX, XXX,            XXX,  KC_UP,
+                                           KC_PGDN,  KC_PGUP,             XXX, XXX
+  )
+};
+
+
+void keyboard_post_init_user(void) {
+    #ifdef CONSOLE_ENABLE
+        debug_enable=true;
+        debug_matrix=true;
+        debug_keyboard=true;
+    #endif
+}
+
+uint8_t prev_rgblight_mode = RGBLIGHT_DEFAULT_MODE;
+bool led_update_user(led_t led_state) {
+    if(led_state.caps_lock){
+        prev_rgblight_mode = rgblight_get_mode();
+        rgblight_sethsv_noeeprom(0, RGBLIGHT_DEFAULT_SAT, 255);
+        rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+2);
+    }else{
+        rgblight_mode_noeeprom(RGBLIGHT_DEFAULT_MODE);
+    }
+
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+
+    if(is_keyboard_left()){
+        dprintf("Is LEFT part\n");
+    }else{
+        dprintf("Is RIGHT part\n");
+    }
+#endif
+
+    if(is_keyboard_left()){
+        if(matrix_get_row(5) & (1 << 3)){
+            if(matrix_get_row(4) & (1 << 4)){
+                eeconfig_disable();
+                bootloader_jump();
+                return false;
+            }
+        }
+    } else {
+        if(matrix_get_row(10) & (1 << 1)){
+            if(matrix_get_row(11) & (1 << 2)){
+                eeconfig_disable();
+                bootloader_jump();
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
